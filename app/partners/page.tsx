4bdb3319182b57b45bd4a052272e357a08e1a07f
vamshichi <chvamshi03@example.com>
2024-11-07 18:50:40 +0530

    added animaction for herosection

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import Image from "next/image";
 // import Logo from '@/app/images/white logo.png'
-import AboutHeroSection from "../components/AboutHerosection";
+// import AboutHeroSection from "../components/AboutHerosection";
 import PartnersHeroSection from "../components/PartnersHeroSection";
 
 const AboutPage = () => {
