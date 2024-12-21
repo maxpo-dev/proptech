bbe868e9353ac302820cb71ddf2db1d36f4526bf
vamshichi <chvamshi03@example.com>
2024-12-21 15:48:45 +0530

    first commit

--- app/components/dubai-section.tsx
+++ app/components/dubai-section.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MapPin } from 'lucide-react'
+// import { MapPin } from 'lucide-react'
 // import Link from "next/link"
 import Image from "next/image"
 import DubaiImg from "@/app/images/Dubai.jpg"
