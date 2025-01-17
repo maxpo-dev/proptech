5e8b80fe8878a5c9bd0be60772e62ae74aa8f814
vamshichi <chvamshi03@example.com>
2025-01-17 13:26:46 +0530

    added google ants

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,8 +47,14 @@ fbq('track', 'PageView');
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              'ad_user_data': 'denied',
+              'ad_personalization': 'denied',
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied',
+              'wait_for_update': 500,
+            });
             gtag('js', new Date());
-
             gtag('config', 'G-GV1DBDBMMV');
           `}
         </Script>
