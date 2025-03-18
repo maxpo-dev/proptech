7eb431b9fcdd44a9dbaf3714e1047ad99f1750a1
maxpo-dev <dev.maxpo@gmail.com>
2025-03-18 16:44:04 +0530

    add pdf for sponsorship brochure

--- app/sponsorship-brochure/page.tsx
+++ app/sponsorship-brochure/page.tsx
@@ -1,38 +1,32 @@
 import React from 'react';
-// import Image from 'next/image';
-// import sponsorImg1 from '@/public/image/brochure/sponsorship brochure 1_page-0001.jpg';
-// import sponsorImg2 from '@/public/image/brochure/sponsorship brochure 1_page-0002.jpg';
+import Image from 'next/image';
+import sponsorImg1 from '@/public/image/brochure/Sponsorship Brochure_Updated-images-0.jpg';
+import sponsorImg2 from '@/public/image/brochure/Sponsorship Brochure_Updated-images-1.jpg';
 
 const SponsorshipBrochure: React.FC = () => {
   return (
-    <div>
-      <iframe
-  src="https://futureproptechsummit.com/sponsorship-brochure.pdf"
-  width="100%"
-  height="600px"
-/>
-
-    {/* //   style={{
-    //     maxWidth: '600px',
-    //     margin: 'calc(2rem + 80px) auto 0', // Extra top margin for the fixed navbar....
-    //     padding: '2rem',
-    //     textAlign: 'center',
-    //   }}
-    // >
-    //   <Image
-    //     src={sponsorImg1}
-    //     alt="Sponsorship Brochure"
-    //     width={600}
-    //     height={480}
-    //     style={{ display: 'block', margin: '0 auto' }}
-    //   />
-    //   <Image
-    //     src={sponsorImg2}
-    //     alt="Sponsorship Brochure"
-    //     width={600}
-    //     height={480}
-    //     style={{ display: 'block', margin: '0.1rem auto 0' }}
-    //   /> */}
+    <div className='py-20'
+      style={{
+        // maxWidth: '600px',
+        // margin: 'calc(2rem + 80px) auto 0', // Extra top margin for the fixed navbar....
+        // padding: '2rem',
+        // textAlign: 'center',
+      }}
+    >
+      <Image
+        src={sponsorImg1}
+        alt="Sponsorship Brochure"
+        // width={600}
+        // height={480}
+        style={{ display: 'block', margin: '0 auto' }}
+      />
+      <Image
+        src={sponsorImg2}
+        alt="Sponsorship Brochure"
+        // width={600}
+        // height={480}
+        style={{ display: 'block', margin: '0.1rem auto 0' }}
+      />
     </div>
   );
 };
