893e2af8dfdb7028d13d2ad530f5674a66d48b59
rohan654321 <mondalrohan201@gmail.com>
2025-04-25 11:24:18 +0530

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,35 +1,16 @@
-// app/register/page.tsx
-
 'use client'
 
-// import EventContent from '../components/EventInfo';
-// import ExhibitorForm from '../components/exhibitor';
-import ContactUs from '../components/reg';
-// import RegisterHeroSection from '../components/RegisterHerosection';
+import { Suspense } from 'react'
+import ContactUs from '../components/reg'
 
 export default function Register() {
   return (
     <div>
-      {/* <RegisterHeroSection /> */}
-      {/* <div className='bg-slate-200'> */}
-      {/* <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-20 animate-fadeInContent py-14"> */}
-        {/* Grid layout for the hero section */}
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 h-full"> */}
-          
-          {/* Left Side: EventInfo Components */}
-          <div className="pt-10">
-            {/* <EventContent /> */}
-          </div>
-
-          {/* Right Side: RegisterForm or other content */}
-          {/* <div className="flex flex-col h-full p-6  rounded-lg">
-            <ExhibitorForm /> {/* Your RegisterForm component */}
-          {/* </div>  */}
+      <div className="pt-10">
+        <Suspense fallback={<div>Loading...</div>}>
           <ContactUs />
-
-        </div>
-      // </div>
-      // </div>
-    // </div> 
+        </Suspense>
+      </div>
+    </div>
   )
 }
