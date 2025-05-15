287dc8d9b5f55d35e76c17069aaaca4c15fd7c09
Sandeep1816 <sandeepavala2@gmail.com>
2025-05-15 16:09:27 +0530

    blog

--- app/components/pitch-benefits.tsx
+++ app/components/pitch-benefits.tsx
@@ -47,7 +47,7 @@ export default function PitchBenefits() {
         {/* CTA Button */}
         <div className="text-center mt-8">
           <a
-            href="http://localhost:3000/register?type=exhibitors"
+            href="/register?type=sponsor"
             className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition duration-300"
           >
             Register as Investor
