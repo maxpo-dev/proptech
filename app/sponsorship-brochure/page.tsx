58df573b422df0862b3a42455682b13a79f8cc5b
Sandeep1816 <sandeepavala2@gmail.com>
2025-06-05 12:12:19 +0530

    sponsorship-brochure

--- app/sponsorship-brochure/page.tsx
+++ app/sponsorship-brochure/page.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
-import sponsorImg1 from '@/public/image/brochure/sponcership brochure-01 (1).jpg';
-import sponsorImg2 from '@/public/image/brochure/sponcership brochure-02 (1).jpg';
+import sponsorImg1 from '@/public/image/brochure/fps-d1.jpg';
+import sponsorImg2 from '@/public/image/brochure/fps-d2.jpg';
 
 const SponsorshipBrochure: React.FC = () => {
   return (
-    <div className='py-20'
-      style={{
-        // maxWidth: '600px',
-        // margin: 'calc(2rem + 80px) auto 0', // Extra top margin for the fixed navbar....
-        // padding: '2rem',
-        // textAlign: 'center',
-      }}
-    >
+    <div className="pt-[100px] pb-20"> {/* Adjusted top padding */}
       <Image
         src={sponsorImg1}
         alt="Sponsorship Brochure"
-        // width={600}
-        // height={480}
         style={{ display: 'block', margin: '0 auto' }}
       />
       <Image
         src={sponsorImg2}
         alt="Sponsorship Brochure"
-        // width={600}
-        // height={480}
         style={{ display: 'block', margin: '0.1rem auto 0' }}
       />
     </div>
